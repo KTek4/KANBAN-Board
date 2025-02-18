1d6a170f8b3660f1a1719e4230a9c633a2c7b07a
krana <kranat10@gmail.com>
2025-02-18 14:56:24 -0600

    client code added

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,29 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
+  try {
+    const resp = await fetch("auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginData),
+    });
 
+    const loginResponse = await resp.json();
 
+    // if there is an error in the response, throw an error. typically this is due to login info being incorrect, so we specify that the cause is likely that
+    if (!resp.ok) {
+      throw new Error(
+        "Error in fetch response, is the users login information correct? Check network tab for more information"
+      );
+    }
+
+    return loginResponse;
+  } catch (err: any) {
+    console.error("Error when attempting to authenticate user:", err);
+    return Promise.reject("Is the users login information correct?");
+  }
+};
 
 export { login };
--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,45 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import type { UserData } from '../interfaces/UserData';
 
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken());
   }
 
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
+    try {
+      const decodedToken = jwtDecode<JwtPayload>(token);
+
+      if (decodedToken?.exp && decodedToken?.exp < Date.now() / 1000) {
+        return true;
+      } else {
+        return false;
+      }
+
+    } catch (err: any) {
+      console.error('There was an error decoding token: ', err);
+      return false;
+    }
   }
 
   getToken(): string {
-    // TODO: return the token
+    const currentUser = localStorage.getItem('id_token') || '';
+    return currentUser;
   }
 
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
